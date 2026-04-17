e624b0b832e5b81d0983193ce058d3dea8e6a8eb
rudakova06polina-netizen <rudakova06polina@gmail.com>
2026-04-17 23:43:14 +0700

    ready

--- part1/part1_2/GradeChecker.java
+++ part1/part1_2/GradeChecker.java
@@ -6,16 +6,31 @@ public class GradeChecker {
         // TODO: реализуйте вариант через классический switch.
         // Подсказка: удобно сначала вычислить "десяток" score / 10.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
-        // ▲ КОНЕЦ ВАШЕГО КОДА ▲
+        int tens = score / 10;
+
+        switch (tens) {
+            case 10:
+            case 9: return "Отлично (A)";
+            case 8: return "Хорошо (B)";
+            case 7: return "Удовлетворительно (C)";
+            default: return "Неудовлетворительно (F)";
+        }
     }
+        // ▲ КОНЕЦ ВАШЕГО КОДА ▲
 
     public static String getGradeArrow(int score) {
         // TODO: реализуйте вариант через switch -> (Java 14+).
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
-        // ▲ КОНЕЦ ВАШЕГО КОДА ▲
+        int tens = score / 10;
+
+        switch (tens) {
+            case 10, 9 -> { return "Отлично (A)"; }
+            case 8      -> { return "Хорошо (B)"; }
+            case 7      -> { return "Удовлетворительно (C)"; }
+            default     -> { return "Неудовлетворительно (F)"; }
+        }
     }
+        // ▲ КОНЕЦ ВАШЕГО КОДА ▲
 
     public static void main(String[] args) {
         int[] samples = {95, 85, 73, 62, 45, 100, 0};
